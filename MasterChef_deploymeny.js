e35c5f795ca7e8d48062028bcd1df121b6887f7b
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-09-03 12:55:30 +0200

    Create MasterChef_deploymeny.js

---
+++ MasterChef_deploymeny.js
@@ -0,0 +1,24 @@
+const XdYeToken = artifacts.require("XdYeToken.sol");
+const MasterChef = artifacts.require("MasterChef.sol");
+
+module.exports = async function (deployer, _network, addresses) {
+  const [admin, _] = addresses;
+
+  await deployer.deploy(XdYeToken);
+  const xdyeToken = await XdYeToken.deployed();
+  
+  const startBlock = 1651184573; 
+  const endBlock =  1659218400;
+
+  await deployer.deploy(
+    MasterChef,
+    xdyeToken.address, 
+    admin,
+    web3.utils.toWei("100"),
+    startBlock,
+    endBlock
+  );
+
+  const masterChef = await MasterChef.deployed();
+  
+}
