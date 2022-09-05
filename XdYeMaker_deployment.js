fa2cbcdde78e871d063682562f2f17a892cf9587
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-09-05 18:13:38 +0200

    Create XdYeMaker_deployment.js

---
+++ XdYeMaker_deployment.js
@@ -0,0 +1,42 @@
+const Factory = artifacts.require("uniswapv2/UniswapV2Factory.sol");
+const Router = artifacts.require("uniswapv2/UniswapV2Router02.sol");
+const WETH = artifacts.require("WETH.sol");
+const XdYeToken = artifacts.require("XdYeToken.sol");
+const MasterChef = artifacts.require("MasterChef.sol");
+const XdYeBar = artifacts.require("XdYeBar.sol");
+const XdYeMaker = artifacts.require("XdYeMaker.sol");
+
+
+module.exports = async function (deployer, _network, addresses) {
+  const [admin, _] = addresses;
+
+  await deployer.deploy(WETH);
+  const weth = await WETH.deployed();
+
+  await deployer.deploy(Factory, admin);
+  const factory = await Factory.deployed();
+
+  await deployer.deploy(Router, factory.address, weth.address);
+  const router = await Router.deployed();
+
+  await deployer.deploy(XdYeToken);
+  const xdyeToken = await XdYeToken.deployed();
+
+  await deployer.deploy(XdYeBar, xdyeToken.address); 
+  const xdyeBar = await XdYeBar.deployed();
+
+  await deployer.deploy(
+     XdYeMaker,
+     factory.address,
+     xdyeBar.address,
+     xdyeToken.address, 
+     weth.address,
+     admin,
+     admin
+   );
+  const xdyeMaker = await XdYeMaker.deployed();
+
+
+  await factory.setFeeTo(xdyeMaker.address) 
+
+};
